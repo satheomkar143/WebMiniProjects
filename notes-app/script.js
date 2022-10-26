f11e69c2bf4cd294fa8ddad07498e0f193c3ef43
omkarsathe <satheomkar143@gmail.com>
2022-10-26 22:14:20 +0530

    basic structure of textarea is created

---
+++ notes-app/script.js
@@ -0,0 +1,16 @@
+const notesEl = document.querySelector(".notes");
+const editBtn = document.querySelector(".edit-btn");
+const deleteBtn = document.querySelector(".delete-btn");
+
+const main = notesEl.querySelector(".main");
+const textArea = notesEl.querySelector("textarea");
+
+editBtn.addEventListener("click", () => {
+  main.classList.toggle("hidden");
+  textArea.classList.toggle("hidden");
+});
+
+textArea.addEventListener("input", (e) => {
+  const { value } = e.target;
+  main.innerHTML = marked(value);
+});
