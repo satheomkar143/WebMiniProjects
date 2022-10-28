ede26a15dff14fe2e6d829b5ee3ea066dd0f2c34
omkarsathe <satheomkar143@gmail.com>
2022-10-28 14:45:05 +0530

    color picker feature addedd to toolbox

--- drawing-app/script.js
+++ drawing-app/script.js
@@ -1,7 +1,12 @@
 const canvas = document.getElementById("canvas");
+const increaseBtn = document.getElementById("increase");
+const decreaseBtn = document.getElementById("decrease");
+const brushSize = document.getElementById("size");
+const colorPicker = document.getElementById("color");
+
 const ctx = canvas.getContext("2d");
 
-let size = 3;
+let size = 30;
 let x = (y = 50);
 let isPressed = false;
 
@@ -16,20 +21,47 @@ canvas.addEventListener("mouseup", () => {
 canvas.addEventListener("mousemove", (e) => {
   console.log(e);
 
-  if(isPressed){
+  if (isPressed) {
     const x = e.offsetX;
     const y = e.offsetY;
     drawCircle(x, y);
   }
 });
 
+let color = "black";
+
+colorPicker.addEventListener("change",(e) => {
+  color = e.target.value;
+  // console.log("color = "+color)
+})
+
 function drawCircle(x, y) {
   ctx.beginPath();
   ctx.arc(x, y, size, 0, Math.PI * 2);
   // ctx.stroke();
-  // ctx.fillStyle = "red";
+  ctx.fillStyle = color;
   ctx.fill();
 }
+
+increaseBtn.addEventListener("click", () => {
+  size += 2;
+  
+  if (size >= 50) {
+    size = 50;
+  }
+
+  brushSize.innerText = size;
+});
+
+decreaseBtn.addEventListener("click", () => {
+  size -= 2;
+
+  if (size <=2) {
+    size = 2;
+  }
+
+  brushSize.innerText = size;
+});
 
 // drawCircle(200,200)
 
@@ -40,4 +72,4 @@ function drawCircle(x, y) {
 //   requestAnimationFrame(draw);
 // }
 
-draw();
+// draw();
