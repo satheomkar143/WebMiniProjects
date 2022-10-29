3cdcffbc02a13d7837e4569ec7259bc0763067b7
omkarsathe <satheomkar143@gmail.com>
2022-10-30 00:18:34 +0530

    weather image added

--- weather-app/script.js
+++ weather-app/script.js
@@ -1,18 +1,50 @@
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+
 const apikey = "3265874a2c77ae4a04bb96236a642d2f";
 
-const url = (location) =>
-  `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apikey}`;
+const url = (city) =>
+  `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`;
 
-async function getWeatherByLocation(location) {
-  const resp = await fetch(url(location), { origin: "cors" });
+async function getWeatherByLocation(city) {
+  const resp = await fetch(url(city), { origin: "cors" });
 
   const respData = await resp.json();
 
   console.log(respData);
+
+  addWeatherToPage(respData);
 }
 
-getWeatherByLocation("Kolhapur");
+// getWeatherByLocation("Kolhapur");
 
 function KtoC(K) {
-  return (K - 273.15).toFixed(2);
+  return Math.floor(K - 273.15);
 }
+
+function addWeatherToPage(data) {
+  const temp = KtoC(data.main.temp);
+
+  const weather = document.createElement("div");
+  weather.classList.add("weather");
+
+  weather.innerHTML = `
+  <h2><img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png"> ${temp} ℃ <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png"></h2>
+  <small>${data.weather[0].main}</small>
+  
+  `;
+
+  main.innerHTML = "";
+  main.appendChild(weather);
+  search.value = "";
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const city = search.value;
+
+  if (city) {
+    getWeatherByLocation(city);
+  }
+});
